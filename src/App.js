bf3b8d8c2d115d1072173da1688207c1a5517857
Luke Evarretta <evarretta.luke@gmail.com>
2023-12-20 03:19:37 -0500

    From main pc

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World. Welcom to Roaminate!!!
+          Hello World. Welcome to Roaminate!!!
         </p>
         <a
           className="App-link"
